0909bfbeba69a5de39a54f21af7339c61b601e61
RadhikaChidrawar <chidrawarradhika@gmail.com>
2025-07-13 08:28:06 +0530

    sort Array in descending order using bubble sort and for loop

---
+++ problemDayThree.js
@@ -0,0 +1,16 @@
+// // Sort this array in descending order using any loop:
+
+let arr = [9, 1, 5, 3, 8, 2];
+let n = arr.length;
+
+for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+        if (arr[j] < arr[j + 1]) {
+            let temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+        }
+    }
+}
+
+console.log("Descending Order Array:", arr);
